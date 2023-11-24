df9c17fac85b3df2148ed1b9f2bacb410a1662b0
unknown <conton.jerome@yahoo.fr>
2023-11-24 17:45:25 +0100

    All OK Bugs Fixés Erreur sur le scoll -  Site OK deployé Sans description ni menus

--- app/js/accueil.js
+++ app/js/accueil.js
@@ -1,44 +1,61 @@
 window.addEventListener("scroll", function() {
   let container1 = document.querySelector(".accueil_container1");
-  let windowHeight = window.innerHeight;
-  let container1Top = container1.offsetTop;
+  if (container1) {
+    let windowHeight = window.innerHeight;
+    let container1Top = container1.offsetTop;
 
-  if (window.scrollY > container1Top - windowHeight / 2) {
-    container1.style.opacity = 1;
+    if (window.scrollY > container1Top - windowHeight / 2) {
+      container1.style.opacity = 1;
+    }
   }
 
   let container2 = document.querySelector(".accueil_container2");
-  let container2Top = container2.offsetTop;
+  if (container2) {
+    let windowHeight = window.innerHeight;
+    let container2Top = container2.offsetTop;
 
-  if (window.scrollY > container2Top - windowHeight / 2) {
-    container2.style.opacity = 1;
+    if (window.scrollY > container2Top - windowHeight / 2) {
+      container2.style.opacity = 1;
+    }
   }
 
   let container3 = document.querySelector(".accueil_container3");
-  let container3Top = container3.offsetTop;
+  if (container3) {
+    let windowHeight = window.innerHeight;
+    let container3Top = container3.offsetTop;
 
-  if (window.scrollY > container3Top - windowHeight / 2) {
-    container3.style.opacity = 1;
+    if (window.scrollY > container3Top - windowHeight / 2) {
+      container3.style.opacity = 1;
+    }
   }
 
   let container4 = document.querySelector(".accueil_container4");
-  let container4Top = container4.offsetTop;
+  if (container4) {
+    let windowHeight = window.innerHeight;
+    let container4Top = container4.offsetTop;
 
-  if (window.scrollY > container4Top - windowHeight / 2) {
-    container4.style.opacity = 1;
+    if (window.scrollY > container4Top - windowHeight / 2) {
+      container4.style.opacity = 1;
+    }
   }
 
   let container5 = document.querySelector(".accueil_container5");
-  let container5Top = container5.offsetTop;
+  if (container5) {
+    let windowHeight = window.innerHeight;
+    let container5Top = container5.offsetTop;
 
-  if (window.scrollY > container5Top - windowHeight / 2) {
-    container5.style.opacity = 1;
+    if (window.scrollY > container5Top - windowHeight / 2) {
+      container5.style.opacity = 1;
+    }
   }
 
   let container6 = document.querySelector(".accueil_container6");
-  let container6Top = container6.offsetTop;
+  if (container6) {
+    let windowHeight = window.innerHeight;
+    let container6Top = container6.offsetTop;
 
-  if (window.scrollY > container6Top - windowHeight / 2) {
-    container6.style.opacity = 1;
+    if (window.scrollY > container6Top - windowHeight / 2) {
+      container6.style.opacity = 1;
+    }
   }
 });
--- app/js/index.js
+++ app/js/index.js
@@ -1,34 +1,44 @@
 
 window.addEventListener("scroll", function() {
-  let container2 = document.querySelector(".container2_presentation");
-  let windowHeight = window.innerHeight;
-  let container2Top = container2.offsetTop;
+  
+let container2 = document.querySelector(".container2_presentation");
+  if (container2) {
+    let windowHeight = window.innerHeight;
+    let container2Top = container2.offsetTop;
 
-  if (window.scrollY > container2Top - windowHeight / 2) {
-    container2.style.opacity = 1;
+    if (window.scrollY > container2Top - windowHeight / 2) {
+      container2.style.opacity = 1;
+    }
   }
 
   let container3 = document.querySelector(".container3_presentation");
-  let container3Top = container3.offsetTop;
+  if (container3) {
+    let windowHeight = window.innerHeight;
+    let container3Top = container3.offsetTop;
 
-  if (window.scrollY > container3Top - windowHeight / 2) {
-    container3.style.opacity = 1;
+    if (window.scrollY > container3Top - windowHeight / 2) {
+      container3.style.opacity = 1;
+    }
   }
-
+    
   let container4 = document.querySelector(".container4_presentation");
-  let container4Top = container4.offsetTop;
+  if (container4) {
+    let windowHeight = window.innerHeight;
+    let container4Top = container4.offsetTop;
 
-  if (window.scrollY > container4Top - windowHeight / 2) {
-    container4.style.opacity = 1;
+    if (window.scrollY > container4Top - windowHeight / 2) {
+      container4.style.opacity = 1;
+    }
   }
 
+    
   let container5 = document.querySelector(".container5_presentation");
-  let container5Top = container5.offsetTop;
+  if (container5) {
+    let windowHeight = window.innerHeight;
+    let container5Top = container5.offsetTop;
 
-  if (window.scrollY > container5Top - windowHeight / 2) {
-    container5.style.opacity = 1;
+    if (window.scrollY > container5Top - windowHeight / 2) {
+      container5.style.opacity = 1;
+    }
   }
 });
-
-
-
