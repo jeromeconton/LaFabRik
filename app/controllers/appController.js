841b49c136b5fb702c629fc999d323f4b01d5bec
unknown <conton.jerome@yahoo.fr>
2023-11-24 16:04:50 +0100

    essaie fix bug

--- app/controllers/appController.js
+++ app/controllers/appController.js
@@ -48,7 +48,7 @@ const appController = {
     res.render('footer/cgv');
   },
   mentionslegalesPage(req, res) {
-    res.render('footer/mentionslegales');
+    res.render('footer/cgv');
   },
   pdcPage(req, res) {
     res.render('footer/pdc');
